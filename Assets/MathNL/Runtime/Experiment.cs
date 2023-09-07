2b7864aee2ef7d2c70ee270a4699bf46d1c51af1
nulock <s2002akashi@outlook.jp>
2023-09-07 09:56:01 +0900

    1.0

--- Assets/MathNL/Runtime/Experiment.cs
+++ Assets/MathNL/Runtime/Experiment.cs
@@ -6,6 +6,9 @@ namespace Nulock.Math
 {
     public static partial class MathNL
     {
+        /// <summary>
+        /// ２つのベクトル間の角度を取得します。
+        /// </summary>
         public static float GetAngle(Vector2 from, Vector2 to)
         {
             var dx = to.x - from.x;
@@ -13,19 +16,30 @@ namespace Nulock.Math
             var rad = Mathf.Atan2(dy, dx);
             return rad * Mathf.Rad2Deg;
         }
+        /// <summary>
+        /// 原点から指定したベクトルへの角度を取得します。
+        /// </summary>
         public static float GetAngle(Vector2 to)
         {
             return GetAngle(Vector2.zero, to);
         }
-
+        /// <summary>
+        /// 指定した角度の方向ベクトルを取得します。
+        /// </summary>
         public static Vector2 GetDirection(float angle)
         {
             return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
         }
+        /// <summary>
+        /// ２つのベクトル間の方向ベクトルを取得します。
+        /// </summary>
         public static Vector2 GetDirection(Vector2 from, Vector2 to)
         {
             return GetDirection(GetAngle(from, to));
         }
+        /// <summary>
+        /// どちら側にいるかどうかを取得します。
+        /// </summary>
         public static float GetSide(Vector2 self, Vector2 target, Vector2 dir)
         {
             Vector2 diff = target - self;
@@ -34,48 +48,76 @@ namespace Nulock.Math
 
             float Cross(Vector2 a, Vector2 b) => a.x * b.y - a.y * b.x;
         }
-
+        /// <summary>
+        /// 与えられたリスト内で、指定した目標値に最も近い上の値を取得します。
+        /// </summary>
         public static int ApproxUp(this IEnumerable<int> list, int target)
         {
             int num = list.Select(x => Mathf.Abs(x - target)).Min();
             return list.Contains(target + num) ? target + num : target - num;
         }
+        /// <summary>
+        /// 与えられたリスト内で、指定した目標値に最も近い上の値を取得します。
+        /// </summary>
         public static float ApproxUp(this IEnumerable<float> list, float target)
         {
             float num = list.Select(x => Mathf.Abs(x - target)).Min();
             return list.Contains(target + num) ? target + num : target - num;
         }
+        /// <summary>
+        /// 与えられたリスト内で、指定した目標値に最も近い下の値を取得します。
+        /// </summary>
         public static int ApproxDown(this IEnumerable<int> list, int target)
         {
             int num = list.Select(x => Mathf.Abs(x - target)).Min();
             return list.Contains(target - num) ? target - num : target + num;
         }
+        /// <summary>
+        /// 与えられたリスト内で、指定した目標値に最も近い下の値を取得します。
+        /// </summary>
         public static float ApproxDown(this IEnumerable<float> list, float target)
         {
             float num = list.Select(x => Mathf.Abs(x - target)).Min();
             return list.Contains(target - num) ? target - num : target + num;
         }
-
+        /// <summary>
+        /// 与えられたリストの分散を計算します。
+        /// </summary>
         public static float Var(this IEnumerable<float> list)
         {
             return (float)list.Select(x => Mathf.Pow(x - list.Average(), 2)).Average();
         }
+        /// <summary>
+        /// 与えられたリストの標準偏差を計算します。
+        /// </summary>
         public static float Stdev(this IEnumerable<float> list)
         {
             return (float)Mathf.Sqrt(list.Var());
         }
+        /// <summary>
+        /// 与えられたリストの範囲（最大値 - 最小値）を計算します。
+        /// </summary>
         public static float Range(this IEnumerable<float> list)
         {
             return list.Max() - list.Min();
         }
-        public static bool WithinRange(this float self, float from, float to)
+        /// <summary>
+        /// 指定した範囲内に値が存在するかどうかを判定します。
+        /// </summary>
+        public static bool IsWithinRange(this float self, float from, float to)
         {
             var (min, max) = (from > to) ? (to, from) : (from, to);
             return min <= self && self <= max;
         }
 
-        //未検証
-        public static bool WithinDirectionRange(Vector2 center,Vector2 current, Vector2 rangeA, Vector2 rangeB)
+        /// <summary>
+        /// 指定した範囲内にベクトルが存在するかどうかを判定します。(実験中)
+        /// </summary>
+        /// <param name="center">中心点</param>
+        /// <param name="current">現在のベクトル</param>
+        /// <param name="rangeA">範囲A</param>
+        /// <param name="rangeB">範囲B</param>
+        public static bool IsWithinDirectionRange(Vector2 center,Vector2 current, Vector2 rangeA, Vector2 rangeB)
         {
             float rangeAAngle = GetAngle(center,rangeA);
             float rangeBAngle = GetAngle(center, rangeB);
@@ -84,19 +126,30 @@ namespace Nulock.Math
             float targetAngle = AverageAngle(rangeAAngle, rangeBAngle);
             float range = AbsoluteAngleDifference(rangeAAngle, rangeBAngle);
 
-            return WithinCircularRange(currentAngle, targetAngle, range);
+            return IsWithinCircularRange(currentAngle, targetAngle, range);
         }
 
-        //未検証
-        public static bool WithinSharpFunRange(Vector2 center, Vector2 current, Vector2 rangeA, Vector2 rangeB,float r)
+        /// <summary>
+        /// 指定した範囲および半径内にベクトルが存在するかどうかを判定します。(実験中)
+        /// </summary>
+        /// <param name="center">中心点</param>
+        /// <param name="current">現在のベクトル</param>
+        /// <param name="rangeA">範囲A</param>
+        /// <param name="rangeB">範囲B</param>
+        /// <param name="r">半径</param>
+        public static bool IsWithinSharpFunRange(Vector2 center, Vector2 current, Vector2 rangeA, Vector2 rangeB,float r)
         {
-            if(Vector2.Distance(center, current) > r) return false;
+            if (!IsWithinCircleRange(center, current, r)) return false;
 
-            return WithinDirectionRange(center, current, rangeA, rangeB);
+            return IsWithinDirectionRange(center, current, rangeA, rangeB);
         }
-        public static bool WithinCircleRange(this Vector2 self, Vector2 target, float r)
+
+        /// <summary>
+        /// 指定した円内にベクトルが存在するかどうかを判定します。
+        /// </summary>
+        public static bool IsWithinCircleRange(Vector2 center, Vector2 current, float r)
         {
-            return Vector2.Distance(self, target) <= r;
+            return Vector2.Distance(center, current) <= r;
         }
     }
 }
--- Assets/MathNL/Runtime/Within.cs
+++ Assets/MathNL/Runtime/Within.cs
@@ -11,7 +11,7 @@ namespace Nulock.Math
         /// <param name="targetAngle">希望の範囲の中心角度。</param>
         /// <param name="rangeAngle">目標角度の周りの希望の範囲の幅。</param>
         /// <returns>現在の角度が目標角度の指定範囲内にある場合はtrueを返し、それ以外の場合はfalseを返します。</returns>
-        public static bool WithinCircularRange(float currentAngle, float targetAngle, float rangeAngle)
+        public static bool IsWithinCircularRange(float currentAngle, float targetAngle, float rangeAngle)
         {
             float diff = AbsoluteAngleDifference(currentAngle, targetAngle);
             return diff <= rangeAngle / 2;
